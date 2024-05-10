b2e971105e5d01793c38a6b7ed67b02d32e4978c
hanawalid222 <hanawalid242@gmail.com>
2024-05-10 22:38:43 +0300

    merged all files (css,html and js) into one html file

--- Kiraric Free Website Template - Free-CSS.com/kiraric/Admin/donors.js
+++ Kiraric Free Website Template - Free-CSS.com/kiraric/Admin/donors.js
@@ -1,8 +1,7 @@
 function viewDownloadInfo(id, firstName, lastName, job) {
     if (job === 'Doctor' || job === 'Teacher') {
         alert(`View/Download Info for Donor:\nID: ${id}\nName: ${firstName} ${lastName}\nJob: ${job}`);
-        // Add functionality to view and download info here
-    } else {
+     } else {
         alert(`This donor's information is not available for viewing/download.`);
     }
 }
