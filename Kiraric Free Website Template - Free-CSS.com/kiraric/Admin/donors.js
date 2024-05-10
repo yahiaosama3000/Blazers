e3a3d690ba850ba06c42afbddc64bd3b3ad678e6
hanawalid222 <hanawalid242@gmail.com>
2024-05-10 22:40:07 +0300

    deleted unused files

--- Kiraric Free Website Template - Free-CSS.com/kiraric/Admin/donors.js
+++
@@ -1,7 +0,0 @@
-function viewDownloadInfo(id, firstName, lastName, job) {
-    if (job === 'Doctor' || job === 'Teacher') {
-        alert(`View/Download Info for Donor:\nID: ${id}\nName: ${firstName} ${lastName}\nJob: ${job}`);
-     } else {
-        alert(`This donor's information is not available for viewing/download.`);
-    }
-}
